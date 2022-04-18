e05af595fba39fb08afa67adf0d69f69f9679e81
benazir <benazirkhakimova91@gmail.com>
2022-04-18 15:26:08 +0600

    added auth

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,38 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  const { user, checkAuth, logout, loading } = useAuthContext();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (localStorage.getItem("token")) {
-      checkAuth();
-    }
-  }, []);
-  return loading ? (
-    <h1>Loading</h1>
-  ) : (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "flex-end",
-        alignItems: "center",
-        marginRight: "10vw",
-      }}
-    >
-      {user ? (
-        <h2>
-          User: {user}{" "}
-          <Button variant="outlined" onClick={logout}>
-            Logout
-          </Button>
-        </h2>
-      ) : (
-        <Button variant="outlined" onClick={() => navigate("/signup")}>
-          Register
-        </Button>
-      )}
-    </div>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
